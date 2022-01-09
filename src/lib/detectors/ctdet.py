ef50d94a21efc974d303c653441a8ad1988ba4c9
Joshua Ernest <90004875+JoshuaErne@users.noreply.github.com>
2022-01-08 17:43:31 -0800

    Add files via upload

---
+++ src/lib/detectors/ctdet.py
@@ -0,0 +1,230 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import numpy as np
+from progress.bar import Bar
+import time
+import torch
+
+try:
+  from external.nms import soft_nms
+except:
+  print('NMS not imported! If you need it,'
+        ' do \n cd $CenterNet_ROOT/src/lib/external \n make')
+from models.decode import ctdet_decode
+from models.utils import flip_tensor
+from utils.image import get_affine_transform
+from utils.post_process import ctdet_post_process
+from utils.debugger import Debugger
+
+from .base_detector import BaseDetector
+
+class CtdetDetector(BaseDetector):
+  def __init__(self, opt):
+    super(CtdetDetector, self).__init__(opt)
+  
+  def process(self, images, return_time=False):
+    with torch.no_grad():
+      output = self.model(images)[-1]
+      hm = output['hm'].sigmoid_()
+      wh = output['wh']
+      reg = output['reg'] if self.opt.reg_offset else None
+      if self.opt.flip_test:
+        hm = (hm[0:1] + flip_tensor(hm[1:2])) / 2
+        wh = (wh[0:1] + flip_tensor(wh[1:2])) / 2
+        reg = reg[0:1] if reg is not None else None
+      torch.cuda.synchronize()
+      forward_time = time.time()
+      dets = ctdet_decode(hm, wh, reg=reg, cat_spec_wh=self.opt.cat_spec_wh, K=self.opt.K)
+      
+    if return_time:
+      return output, dets, forward_time
+    else:
+      return output, dets
+
+  def post_process(self, dets, meta, scale=1):
+    dets = dets.detach().cpu().numpy()
+    dets = dets.reshape(1, -1, dets.shape[2])
+    dets = ctdet_post_process(
+        dets.copy(), [meta['c']], [meta['s']],
+        meta['out_height'], meta['out_width'], self.opt.num_classes)
+    for j in range(1, self.num_classes + 1):
+      dets[0][j] = np.array(dets[0][j], dtype=np.float32).reshape(-1, 5)
+      dets[0][j][:, :4] /= scale
+    return dets[0]
+
+  def merge_outputs(self, detections):
+    results = {}
+    for j in range(1, self.num_classes + 1):
+      results[j] = np.concatenate(
+        [detection[j] for detection in detections], axis=0).astype(np.float32)
+      if len(self.scales) > 1 or self.opt.nms:
+         soft_nms(results[j], Nt=0.5, method=2)
+    scores = np.hstack(
+      [results[j][:, 4] for j in range(1, self.num_classes + 1)])
+    if len(scores) > self.max_per_image:
+      kth = len(scores) - self.max_per_image
+      thresh = np.partition(scores, kth)[kth]
+      for j in range(1, self.num_classes + 1):
+        keep_inds = (results[j][:, 4] >= thresh)
+        results[j] = results[j][keep_inds]
+    return results
+
+  def debug(self, debugger, images, dets, output, scale=1):
+    detection = dets.detach().cpu().numpy().copy()
+    detection[:, :, :4] *= self.opt.down_ratio
+    for i in range(1):
+      img = images[i].detach().cpu().numpy().transpose(1, 2, 0)
+      img = ((img * self.std + self.mean) * 255).astype(np.uint8)
+      pred = debugger.gen_colormap(output['hm'][i].detach().cpu().numpy())
+      debugger.add_blend_img(img, pred, 'pred_hm_{:.1f}'.format(scale))
+      debugger.add_img(img, img_id='out_pred_{:.1f}'.format(scale))
+      for k in range(len(dets[i])):
+        if detection[i, k, 4] > self.opt.center_thresh:
+          debugger.add_coco_bbox(detection[i, k, :4], detection[i, k, -1],
+                                 detection[i, k, 4], 
+                                 img_id='out_pred_{:.1f}'.format(scale))
+
+  def custom_debug(self, debugger, images, dets, output, scale=1):
+    detection = dets.detach().cpu().numpy().copy()
+    detection[:, :, :4] *= self.opt.down_ratio
+    for i in range(1):
+      img = images[i].detach().cpu().numpy().transpose(1, 2, 0)
+      img = ((img * self.std + self.mean) * 255).astype(np.uint8)
+      pred = debugger.gen_colormap(output['hm'][i].detach().cpu().numpy())
+      debugger.add_blend_img(img, pred, 'pred_hm_{:.1f}'.format(scale))
+      debugger.add_img(img, img_id='out_pred_{:.1f}'.format(scale))
+      for k in range(len(dets[i])):
+        if detection[i, k, 4] > self.opt.center_thresh:
+          debugger.add_custom_coco_bbox(detection[i, k, :4], detection[i, k, -1],
+                                        detection[i, k, 4], 
+                                        img_id='out_pred_{:.1f}'.format(scale))
+
+  def show_results(self, debugger, image, results):
+    debugger.add_img(image, img_id='ctdet')
+    for j in range(1, self.num_classes + 1):
+      for bbox in results[j]:
+        if bbox[4] > self.opt.vis_thresh:
+          debugger.add_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
+    debugger.show_all_imgs(pause=self.pause)
+
+  def custom_show_results(self, debugger, image, results):
+    debugger.add_img(image, img_id='ctdet')
+    for j in range(1, self.num_classes + 1):
+      for bbox in results[j]:
+        if bbox[4] > self.opt.vis_thresh:
+          debugger.add_custom_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
+    debugger.show_all_imgs(pause=self.pause)
+
+  def update_boxes(self, new_centers, old_results, indices):
+    for center_idx, index in enumerate(indices):
+      try:
+        image_class, box_index = index
+        bbox = old_results[image_class][box_index]
+        
+        # Get old bbox positions
+        (x1_old, y1_old, x2_old, y2_old) = bbox[:4]
+        
+        # Get bbox height and width
+        h      = x2_old - x1_old
+        w      = y2_old - y1_old
+        
+        # Get new center's x and y
+        cent_x = new_centers[center_idx,0]
+        cent_y = new_centers[center_idx,1]
+
+        # calculate new bbox positions
+        x1_new = cent_x - h//2
+        x2_new = cent_x + h//2
+        y1_new = cent_y - w//2
+        y2_new = cent_y + w//2
+
+        # Update bbox
+        bbox[:4] = (x1_new, y1_new, x2_new, y2_new)
+      except:
+        continue
+
+  def create_detections_mask(self, debugger, mask, results):
+    debugger.add_img(mask, img_id='ctdet')
+    centers = []
+    indices = []
+    
+    for j in range(1, self.num_classes + 1):
+      if debugger.names[j-1] is not None:
+        for index, bbox in enumerate(results[j]):
+          if bbox[4] > self.opt.vis_thresh:
+            debugger.add_coco_mask(bbox[:4], j - 1, bbox[4], img_id='ctdet')
+
+            center       = np.array([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2]).reshape(1,2).astype(np.float32)
+
+            centers.append(center)
+            indices.append((j,index))
+    # debugger.show_all_imgs(pause=self.pause)
+    centers = np.array(centers)
+    return debugger.imgs['ctdet'], centers, indices
+
+  def run(self, image_or_path_or_tensor, meta=None):
+    load_time, pre_time, net_time, dec_time, post_time = 0, 0, 0, 0, 0
+    merge_time, tot_time = 0, 0
+    debugger = Debugger(dataset='custom', ipynb=(self.opt.debug==3),
+                        theme=self.opt.debugger_theme)
+    start_time = time.time()
+    pre_processed = False
+    if isinstance(image_or_path_or_tensor, np.ndarray):
+      image = image_or_path_or_tensor
+    elif type(image_or_path_or_tensor) == type (''): 
+      image = cv2.imread(image_or_path_or_tensor)
+    else:
+      image = image_or_path_or_tensor['image'][0].numpy()
+      pre_processed_images = image_or_path_or_tensor
+      pre_processed = True
+    
+    loaded_time = time.time()
+    load_time += (loaded_time - start_time)
+    
+    detections = []
+    for scale in self.scales:
+      scale_start_time = time.time()
+      if not pre_processed:
+        images, meta = self.pre_process(image, scale, meta)
+      else:
+        # import pdb; pdb.set_trace()
+        images = pre_processed_images['images'][scale][0]
+        meta = pre_processed_images['meta'][scale]
+        meta = {k: v.numpy()[0] for k, v in meta.items()}
+      images = images.to(self.opt.device)
+      torch.cuda.synchronize()
+      pre_process_time = time.time()
+      pre_time += pre_process_time - scale_start_time
+      
+      output, dets, forward_time = self.process(images, return_time=True)
+
+      torch.cuda.synchronize()
+      net_time += forward_time - pre_process_time
+      decode_time = time.time()
+      dec_time += decode_time - forward_time
+      
+      if self.opt.debug >= 2:
+        self.debug(debugger, images, dets, output, scale)
+      
+      dets = self.post_process(dets, meta, scale)
+      torch.cuda.synchronize()
+      post_process_time = time.time()
+      post_time += post_process_time - decode_time
+
+      detections.append(dets)
+    
+    results = self.merge_outputs(detections)
+    torch.cuda.synchronize()
+    end_time = time.time()
+    merge_time += end_time - post_process_time
+    tot_time += end_time - start_time
+
+    if self.opt.debug >= 1:
+      self.custom_show_results(debugger, image, results)
+    
+    return {'results': results, 'tot': tot_time, 'load': load_time,
+            'pre': pre_time, 'net': net_time, 'dec': dec_time,
+            'post': post_time, 'merge': merge_time}, debugger
